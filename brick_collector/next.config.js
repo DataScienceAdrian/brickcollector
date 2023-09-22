60fefee5f6b37bf6ef5c522508b39fb148f0943a
Adrian Marcinczyk <amarcinczyk@objectivity.co.uk>
2023-09-22 09:14:20 +0200

    Another fix

--- brick_collector/next.config.js
+++ brick_collector/next.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/datascienceadrian.github.io/brickcollector/gh-pages/' : '',
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/DataScienceAdrian/datascienceadrian.github.io/brickcollector/gh-pages/' : '',
 }
 
 //test
